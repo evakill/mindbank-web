107aae02e2f0e6ba7adc359a327c5d1d4b03272f
Eva Killenberg <evakill@seas.upenn.edu>
2018-12-22 11:41:03 -0500

    typo

--- components/Header.js
+++ components/Header.js
@@ -33,7 +33,7 @@ class Header extends React.Component {
             onMouseEnter={() => this.setState({ hovering: true })}
             onMouseLeave={() => this.setState({ hovering: false })}>
           <img
-            src="/static/img/favicon.io"
+            src="/static/img/favicon.ico"
             alt="The MindBank"
             width="45"
             height="45"/>
